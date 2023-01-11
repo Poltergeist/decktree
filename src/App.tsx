16b4e7f8e720facfe82f975c0c976f4585c3289b
Poltergeist <spectator.networx@gmail.com>
2023-01-11 22:03:55 +0100

    feat: add pretty quick and logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import "./App.css";
 import Deck from "./Deck";
 import data from "./data/data.json";
-import {type MoxfieldDeck} from "./deck-types";
+import logo from "./logo.png";
+import { type MoxfieldDeck } from "./deck-types";
 
 function App() {
-  const decks  = data.decks;
-  console.log(decks);
+  const decks = data.decks;
   return (
     <div className="App">
+      <header>
+        <img src={logo} alt="decktree" />
+      </header>
       <section className="deck-grid">
-        {decks.map((deck:MoxfieldDeck) => <Deck key={`deck-${deck.id}`} deck={deck}/>)}
+        {decks.map((deck: MoxfieldDeck) => (
+          <Deck key={`deck-${deck.id}`} deck={deck} />
+        ))}
       </section>
     </div>
   );
--- src/Deck.tsx
+++ src/Deck.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
-import './Deck.css';
-import {type MoxfieldDeck} from "./deck-types";
+import React from "react";
+import "./Deck.css";
+import { type MoxfieldDeck } from "./deck-types";
 
+function Deck({ deck }: { deck: MoxfieldDeck }) {
+  const commanders = deck.commanders;
+  const companions = deck.companions;
 
-function Deck({deck}: {deck: MoxfieldDeck}) {
-
-  const commanders = deck.commanders 
-  const companions = deck.companions
-
-
-
-console.log(deck);
   return (
     <div className="deck">
       <header className="deck-header">
-        <section className={`deck-logo commander${commanders.length}-companion${companions.length}`}>
-          {commanders.map(commander => <section>
-          <div className="image" style={{backgroundImage: `url(${commander.art_crop})`}}></div>
-          <p className='artist-attribution'>{commander.artist}</p>
-          </section>)}
-          {companions.map(companion => <section>
-          <div className="image" style={{backgroundImage: `url(${companion.art_crop})`}}></div>
-          <p className='artist-attribution'>{companion.artist}</p>
-          </section>)}
+        <section
+          className={`deck-logo commander${commanders.length}-companion${companions.length}`}
+        >
+          {commanders.map((commander) => (
+            <section>
+              <div
+                className="image"
+                style={{ backgroundImage: `url(${commander.art_crop})` }}
+              ></div>
+              <p className="artist-attribution">{commander.artist}</p>
+            </section>
+          ))}
+          {companions.map((companion) => (
+            <section>
+              <div
+                className="image"
+                style={{ backgroundImage: `url(${companion.art_crop})` }}
+              ></div>
+              <p className="artist-attribution">{companion.artist}</p>
+            </section>
+          ))}
         </section>
-        <h3>
-          {deck.name}
-        </h3>
-        <p>
-          {deck.description}
-        </p>
+        <h3>{deck.name}</h3>
+        <p>{deck.description}</p>
       </header>
     </div>
   );
